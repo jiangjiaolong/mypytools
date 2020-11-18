a0b91430cf5570c71ad981b86c4385b197b6a75e
jiangjiaolong <35860487+jiangjiaolong@users.noreply.github.com>
2020-11-18 10:46:35 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mytools',
+      version="1.0.0",
+      # description="mytools",
+      author='jiang',
+      platforms=["any"],
+      # author_email='',
+      # url='',
+      py_modules=['mytools']
+      )
